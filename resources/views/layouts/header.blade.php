a6ba0e5123515f5e7c8ec45d40cc01bcfbcddaa3
pratiksha187 <pratikshanmisal@gmail.com>
2025-11-21 10:43:07 +0530

    chnages

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,157 +1,155 @@
 <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
 
-  <header x-data="{ navOpen: false }" class="bg-white shadow sticky top-0 z-50">
+<!-- HEADER -->
+<header x-data="{ openMenu: false }" class="sticky top-0 z-50 bg-white shadow-lg">
 
-  <nav class="bg-white shadow" x-data="{ servicesOpen: false, roadOpen: false, infraOpen: false, landOpen: false, waterOpen: false, compoundOpen: false, residentialOpen: false, retrofitOpen: false }">
-    <div class="container mx-auto px-4 py-4 flex justify-between items-center">
-
-      <!-- Logo -->
-      <a href="{{ url('/') }}" class="flex items-center space-x-3">
-        <img src="{{ asset('storage/logo/logor.png') }}" alt="Logo" class="h-16 w-auto object-contain">
-      </a>
-
-      <!-- Navigation -->
-      <ul class="flex space-x-6 items-center">
-        <li><a href="{{ route('home') }}" class="text-gray-700 hover:text-yellow-600 font-medium">Home</a></li>
-        <li><a href="{{ route('about') }}" class="text-gray-700 hover:text-yellow-600 font-medium">About</a></li>
-
-        <!-- Services Dropdown -->
-        <li class="relative" @click.outside="servicesOpen = false; roadOpen = false; infraOpen = false; landOpen = false; waterOpen = false; compoundOpen = false; residentialOpen = false; retrofitOpen = false;">
-          <button @click="servicesOpen = !servicesOpen" class="text-gray-700 hover:text-yellow-600 font-medium flex items-center focus:outline-none">
-            Services
-            <svg class="ml-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M5.23 7.21a.75.75 0 011.06.02L10 10.94l3.71-3.71a.75.75 0 111.06 1.06l-4.24 4.24a.75.75 0 01-1.06 0L5.21 8.29a.75.75 0 01.02-1.08z"/>
-            </svg>
-          </button>
-
-          <!-- Dropdown Menu -->
-          <div x-show="servicesOpen" x-transition x-cloak class="absolute bg-white shadow-lg rounded mt-2 py-2 w-64 z-50">
-
-            <!-- Road Construction -->
-            <div class="relative">
-              <div class="flex justify-between items-center px-4 py-2 hover:bg-yellow-100 text-gray-700 cursor-pointer" @click="roadOpen = !roadOpen">
-                {{-- <span class="flex-1">Road Construction</span> --}}
-                <a href="/road-construction" class="flex items-center space-x-2 hover:text-orange-600">
-                    <span class="flex-1">Road Construction</span>
-                </a>
-
-                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path d="M6 6l6 4-6 4V6z"/></svg>
-              </div>
-              <div x-show="roadOpen" x-transition x-cloak class="absolute left-full top-0 bg-white shadow-lg rounded w-56 z-50">
-                <a href="{{ route('asphalt_paving') }}" class="block px-4 py-2 hover:bg-yellow-100">Asphalt / Bitumen Road</a>
-                <a href="{{ route('inset_concrete_paving') }}" class="block px-4 py-2 hover:bg-yellow-100">Concrete Road (Inset & Offset)</a>
-                <a href="{{ route('asphalt_compaction') }}" class="block px-4 py-2 hover:bg-yellow-100">Asphalt Compaction</a>
-                <a href="{{ route('gsb_wmm_laying') }}" class="block px-4 py-2 hover:bg-yellow-100">GSB & WMM Laying</a>
-                <a href="{{ route('base_layer_subgrade_preparation') }}" class="block px-4 py-2 hover:bg-yellow-100">Base Layer & Subgrade</a>
-              </div>
-            </div>
-
-            <!-- Industrial Infrastructure -->
-            <div class="relative">
-              <div class="flex justify-between items-center px-4 py-2 hover:bg-yellow-100 text-gray-700 cursor-pointer" @click="infraOpen = !infraOpen">
-                {{-- <span class="flex-1">Industrial Infrastructure</span> --}}
-                 <a href="{{ route('industrial_infrastructure') }}" class="flex items-center space-x-2 hover:text-orange-600">
-                    <span class="flex-1">Industrial Infrastructure</span>
-                </a>
-                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path d="M6 6l6 4-6 4V6z"/></svg>
-              </div>
-              <div x-show="infraOpen" x-transition x-cloak class="absolute left-full top-0 bg-white shadow-lg rounded w-56 z-50">
-                <a href="{{ route('industrial_shed_work') }}" class="block px-4 py-2 hover:bg-yellow-100">Industrial Shed Work</a>
-                <a href="{{ route('factory_building_civil_work') }}" class="block px-4 py-2 hover:bg-yellow-100">Factory Building Civil Work</a>
-              </div>
-            </div>
-
-            <!-- Land Development -->
-            <div class="relative">
-              <div class="flex justify-between items-center px-4 py-2 hover:bg-yellow-100 text-gray-700 cursor-pointer" @click="landOpen = !landOpen">
-                {{-- <span class="flex-1">Land Development</span> --}}
-                 <a href="{{ route('land_development') }}" class="flex items-center space-x-2 hover:text-orange-600">
-                    <span class="flex-1">Land Development</span>
-                </a>
-                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path d="M6 6l6 4-6 4V6z"/></svg>
-              </div>
-              <div x-show="landOpen" x-transition x-cloak class="absolute left-full top-0 bg-white shadow-lg rounded w-56 z-50">
-                <a href="{{ route('plot_leveling_grading_road_access') }}" class="block px-4 py-2 hover:bg-yellow-100">Plot Leveling, Grading, Access</a>
-                <a href="{{ route('earth_cutting_filling_compaction') }}" class="block px-4 py-2 hover:bg-yellow-100">Earth Cutting & Compaction</a>
-              </div>
-            </div>
-
-            <!-- Water & Drainage -->
-            <div class="relative">
-              <div class="flex justify-between items-center px-4 py-2 hover:bg-yellow-100 text-gray-700 cursor-pointer" @click="waterOpen = !waterOpen">
-                
-                <a href="{{ route('water_drainage') }}" class="flex items-center space-x-2 hover:text-orange-600">
-                    <span class="flex-1">Water & Drainage</span>
-                </a>
-                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path d="M6 6l6 4-6 4V6z"/></svg>
-              </div>
-              <div x-show="waterOpen" x-transition x-cloak class="absolute left-full top-0 bg-white shadow-lg rounded w-56 z-50">
-                <a href="{{ route('storm_water_drainage') }}" class="block px-4 py-2 hover:bg-yellow-100">Storm Water Drainage</a>
-                <a href="{{ route('underground_water_supply') }}" class="block px-4 py-2 hover:bg-yellow-100">Underground Water Supply</a>
-                <a href="{{ route('drainage_line_sewer_work') }}" class="block px-4 py-2 hover:bg-yellow-100">Drainage & Sewer Work</a>
-                <a href="{{ route('rcc_chamber_installation') }}" class="block px-4 py-2 hover:bg-yellow-100">RCC / Precast Chamber</a>
-                <a href="{{ route('precast_u_drain') }}" class="block px-4 py-2 hover:bg-yellow-100">Precast U Drain</a>
-              </div>
-            </div>
-
-            <!-- Compound Wall -->
-            <div class="relative">
-              <div class="flex justify-between items-center px-4 py-2 hover:bg-yellow-100 text-gray-700 cursor-pointer" @click="compoundOpen = !compoundOpen">
-                {{-- <span class="flex-1">Compound Wall Construction</span> --}}
-                
-                <a href="{{ route('compound_wall') }}" class="flex items-center space-x-2 hover:text-orange-600">
-                    <span class="flex-1">Compound Wall Construction</span>
-                </a>
-                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path d="M6 6l6 4-6 4V6z"/></svg>
-              </div>
-              <div x-show="compoundOpen" x-transition x-cloak class="absolute left-full top-0 bg-white shadow-lg rounded w-56 z-50">
-                <a href="{{ route('ucr_masonry_compound_walls') }}" class="block px-4 py-2 hover:bg-yellow-100">UCR Masonry Compound Walls</a>
-                <a href="{{ route('precast_compound_walls') }}" class="block px-4 py-2 hover:bg-yellow-100">Precast Compound Walls</a>
-                <a href="{{ route('fencing_work') }}" class="block px-4 py-2 hover:bg-yellow-100">Fencing Work</a>
-              </div>
-            </div>
-
-            <!-- Residential & Commercial -->
-            <div class="relative">
-              <div class="flex justify-between items-center px-4 py-2 hover:bg-yellow-100 text-gray-700 cursor-pointer" @click="residentialOpen = !residentialOpen">
-                {{-- <span class="flex-1">Residential & Commercial Civil Work</span> --}}
-                <a href="{{ route('residencial_commercial') }}" class="flex items-center space-x-2 hover:text-orange-600">
-                    <span class="flex-1">Residential & Commercial Civil Work</span>
-                </a>
-                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path d="M6 6l6 4-6 4V6z"/></svg>
-              </div>
-              <div x-show="residentialOpen" x-transition x-cloak class="absolute left-full top-0 bg-white shadow-lg rounded w-56 z-50">
-                <a href="{{ route('bungalow_construction') }}" class="block px-4 py-2 hover:bg-yellow-100">Bungalow Construction</a>
-                <a href="{{ route('commercial_unit_civil_work') }}" class="block px-4 py-2 hover:bg-yellow-100">Commercial Unit Civil Work</a>
-              </div>
-            </div>
-
-            <!-- Retrofitting & Demolition -->
-            <div class="relative">
-              <div class="flex justify-between items-center px-4 py-2 hover:bg-yellow-100 text-gray-700 cursor-pointer" @click="retrofitOpen = !retrofitOpen">
-               
-                <a href="{{ route('retrofiting_demolition') }}" class="flex items-center space-x-2 hover:text-orange-600">
-                    <span class="flex-1">Retrofitting & Demolition</span>
-                </a>
-                <svg class="ml-2 w-4 h-4" fill="currentColor" viewBox="0 0 20 20"><path d="M6 6l6 4-6 4V6z"/></svg>
-              </div>
-              <div x-show="retrofitOpen" x-transition x-cloak class="absolute left-full top-0 bg-white shadow-lg rounded w-56 z-50">
-                <a href="{{ route('structural_modification') }}" class="block px-4 py-2 hover:bg-yellow-100">Structural Modification</a>
-                <a href="{{ route('repair_strengthening_reinforcement') }}" class="block px-4 py-2 hover:bg-yellow-100">Reinforcement & Repairs</a>
-                <a href="{{ route('demolition_work') }}" class="block px-4 py-2 hover:bg-yellow-100">Demolition Work</a>
-              </div>
-            </div>
-
-          </div>
-        </li>
-
-        <li><a href="{{ route('project') }}" class="text-gray-700 hover:text-yellow-600 font-medium">Projects</a></li>
-        <li><a href="{{ route('contact_us') }}" class="text-gray-700 hover:text-yellow-600 font-medium">Contact</a></li>
-      </ul>
-
-      <!-- Quote button -->
-      <a href="{{ route('contact_us') }}" class="bg-yellow-500 hover:bg-yellow-600 text-white px-4 py-2 rounded font-medium">Get Quote</a>
+    <!-- TOP BAR -->
+    <div class="bg-yellow-600 text-white text-sm py-1">
+        <div class="max-w-7xl mx-auto px-4 flex justify-between">
+            <span><i class="fa-solid fa-phone mr-2"></i> +91 90000 00000</span>
+            <span><i class="fa-solid fa-envelope mr-2"></i> info@shreeyashconstruction.com</span>
+        </div>
     </div>
-  </nav>
+
+    <!-- MAIN NAV -->
+    <nav class="max-w-7xl mx-auto px-4 py-4 flex items-center justify-between">
+
+        <!-- Logo -->
+        <a href="{{ url('/') }}" class="flex items-center gap-3">
+            <img src="{{ asset('storage/logo/logor.png') }}" class="h-14 object-contain" alt="Logo">
+            <span class="font-bold text-xl text-gray-800 tracking-wide">Shreeyash Construction</span>
+        </a>
+
+        <!-- Desktop Nav -->
+        <ul class="hidden lg:flex items-center space-x-8 font-medium">
+
+            <li><a href="{{ route('home') }}" class="nav-link">Home</a></li>
+            <li><a href="{{ route('about') }}" class="nav-link">About</a></li>
+
+            <!-- SERVICES DROPDOWN -->
+            <li class="relative" x-data="{ open: false }" @mouseenter="open = true" @mouseleave="open = false">
+                <button class="nav-link flex items-center gap-1">
+                    Services <i class="fa-solid fa-chevron-down text-xs"></i>
+                </button>
+
+                <!-- LARGE MEGA MENU -->
+                <div x-show="open"
+                     x-transition
+                     class="absolute left-0 top-full w-[900px] bg-white shadow-2xl rounded-lg p-6 grid grid-cols-3 gap-6 border">
+
+                    <!-- COL 1 -->
+                    <div>
+                        <h4 class="font-semibold text-gray-700 mb-2"><i class="fa-solid fa-road mr-2 text-yellow-600"></i>Road Construction</h4>
+                        <ul class="space-y-2 text-sm text-gray-700">
+                            <li><a href="{{ route('asphalt_paving') }}" class="dropdown-item">Asphalt / Bitumen Road</a></li>
+                            <li><a href="{{ route('inset_concrete_paving') }}" class="dropdown-item">Concrete Road</a></li>
+                            <li><a href="{{ route('asphalt_compaction') }}" class="dropdown-item">Asphalt Compaction</a></li>
+                            <li><a href="{{ route('gsb_wmm_laying') }}" class="dropdown-item">GSB / WMM Laying</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- COL 2 -->
+                    <div>
+                        <h4 class="font-semibold text-gray-700 mb-2"><i class="fa-solid fa-industry mr-2 text-yellow-600"></i>Industrial Infra</h4>
+                        <ul class="space-y-2 text-sm text-gray-700">
+                            <li><a href="{{ route('industrial_shed_work') }}" class="dropdown-item">Industrial Sheds</a></li>
+                            <li><a href="{{ route('factory_building_civil_work') }}" class="dropdown-item">Factory Building Work</a></li>
+                            <li><a href="{{ route('land_development') }}" class="dropdown-item">Land Development</a></li>
+                            <li><a href="{{ route('earth_cutting_filling_compaction') }}" class="dropdown-item">Earthwork & Compaction</a></li>
+                        </ul>
+                    </div>
+
+                    <!-- COL 3 -->
+                    <div>
+                        <h4 class="font-semibold text-gray-700 mb-2"><i class="fa-solid fa-water mr-2 text-yellow-600"></i>Water & Drainage</h4>
+                        <ul class="space-y-2 text-sm text-gray-700">
+                            <li><a href="{{ route('storm_water_drainage') }}" class="dropdown-item">Storm Water Drainage</a></li>
+                            <li><a href="{{ route('underground_water_supply') }}" class="dropdown-item">Underground Water Supply</a></li>
+                            <li><a href="{{ route('drainage_line_sewer_work') }}" class="dropdown-item">Drainage & Sewer Work</a></li>
+                            <li><a href="{{ route('precast_u_drain') }}" class="dropdown-item">Precast U Drain</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </li>
+
+            <li><a href="{{ route('project') }}" class="nav-link">Projects</a></li>
+            <li><a href="{{ route('contact_us') }}" class="nav-link">Contact</a></li>
+        </ul>
+
+        <!-- CTA BUTTON -->
+        <a href="{{ route('contact_us') }}" class="hidden lg:inline-block bg-yellow-500 hover:bg-yellow-600 text-white px-5 py-2 rounded-lg shadow font-medium transition">
+            Get Quote
+        </a>
+
+        <!-- MOBILE MENU BUTTON -->
+        <button @click="openMenu = !openMenu" class="lg:hidden text-gray-800 text-2xl">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+    </nav>
+
+    <!-- MOBILE MENU -->
+    <div x-show="openMenu" x-transition class="lg:hidden bg-white shadow-inner p-4 space-y-3">
+
+        <a href="{{ route('home') }}" class="mobile-link">Home</a>
+        <a href="{{ route('about') }}" class="mobile-link">About</a>
+
+        <details class="group">
+            <summary class="mobile-link">Services</summary>
+            <div class="pl-4 space-y-2">
+
+                <details>
+                    <summary class="mobile-sub">Road Construction</summary>
+                    <div class="pl-4 space-y-1">
+                        <a href="{{ route('asphalt_paving') }}" class="mobile-item">Asphalt Road</a>
+                        <a href="{{ route('inset_concrete_paving') }}" class="mobile-item">Concrete Road</a>
+                    </div>
+                </details>
+
+                <details>
+                    <summary class="mobile-sub">Industrial Infra</summary>
+                    <div class="pl-4 space-y-1">
+                        <a href="{{ route('industrial_shed_work') }}" class="mobile-item">Industrial Shed</a>
+                        <a href="{{ route('factory_building_civil_work') }}" class="mobile-item">Factory Work</a>
+                    </div>
+                </details>
+
+                <details>
+                    <summary class="mobile-sub">Water & Drainage</summary>
+                    <div class="pl-4 space-y-1">
+                        <a href="{{ route('storm_water_drainage') }}" class="mobile-item">Storm Drainage</a>
+                        <a href="{{ route('underground_water_supply') }}" class="mobile-item">Water Supply</a>
+                    </div>
+                </details>
+
+            </div>
+        </details>
+
+        <a href="{{ route('project') }}" class="mobile-link">Projects</a>
+        <a href="{{ route('contact_us') }}" class="mobile-link">Contact</a>
+
+        <a href="{{ route('contact_us') }}" class="block bg-yellow-500 text-center text-white py-2 rounded-lg font-semibold mt-2">
+            Get Quote
+        </a>
+    </div>
+
 </header>
+
+<!-- EXTRA STYLES -->
+<style>
+    .nav-link {
+        @apply text-gray-700 hover:text-yellow-600 transition font-medium cursor-pointer;
+    }
+    .dropdown-item {
+        @apply hover:text-yellow-600 block py-1 transition;
+    }
+    .mobile-link {
+        @apply block py-2 text-gray-800 font-medium border-b;
+    }
+    .mobile-sub {
+        @apply py-2 text-gray-700 font-semibold cursor-pointer;
+    }
+    .mobile-item {
+        @apply block py-1 text-gray-600;
+    }
+</style>
